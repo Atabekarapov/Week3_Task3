95232c1512a09de4769e92e45fd95c9076f8912a
Atabek <atabekarapov14@gmail.com>
2020-11-02 13:37:27 +0600

    Week3_Task3

---
+++ Task3.py
@@ -0,0 +1,51 @@
+# Task3. HOWEWORK -----> Atabek
+# 1)
+
+# import random
+# nums = [random.randint (0, 100) for i in range(10)]
+# nums2 = [elem for elem in nums if elem%2==0]
+# print(nums2)
+
+# 2)
+# nums = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]
+
+# best = [num**2 for num in nums[0]],[num**2 for num in nums[1]],[num**2 for num in nums[2]]
+
+# print(list(best))
+
+# 3)
+# nums = ['12', '15', '16', '17']
+# nums2 = [int(num) for num in nums]
+# print(nums2)
+
+# 4)
+# nums = {'a': 50, 'b': 3232, 'c': 2314, 'd': 342343}
+# nums_only = nums.values()
+
+# answer = [print('Foo') if x % 3 == 0 else print('Bar') if x % 5 == 0  else None for x in nums_only]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
